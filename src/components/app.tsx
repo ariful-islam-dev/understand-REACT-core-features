5dab9fcf85ce8f920e41ff99e29974f6e6ccd7c9
ariful4082 <ariful4082@gmail.com>
2022-04-05 19:24:22 +0600

    discus root component and eject script

---
+++ src/components/app.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ariful Islam</h1>
+        <h2>Hello React Your are awesome</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
