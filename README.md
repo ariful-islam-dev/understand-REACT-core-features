9bf455a348eceed5cf60efda6f2d0e97a5fa8ca1
ariful4082 <ariful4082@gmail.com>
2022-04-05 14:52:47 +0600

    start project with webpack

---
+++ README.md
@@ -0,0 +1,10 @@
+# Understanding ReactJS Core Features
+
+**_Stack Learner_**
+
+### We are going to learn
+
+- [x] Tools We Need to Up & Running REact
+- [x] BabelJS & Webpack Setup
+- [x] Create Our Custom Environment
+- [x] Create Our First React App
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { hot } from "react-hot-loader/root";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Name: Ariful Islam</h1>
+        <h3>Junior Fullstack Developer</h3>
+      </div>
+    );
+  }
+}
+
+export default hot(App);
