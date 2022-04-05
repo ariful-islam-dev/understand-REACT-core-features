ba62ffd45ffa398d22af3182f6db49dcf54ec18e
ariful4082 <ariful4082@gmail.com>
2022-04-05 22:32:59 +0600

    usecase of jsx

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,11 +2,25 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return (
+    const obj: Object = {
+      title: "Test Title",
+      id: "myh1",
+    };
+
+    const name: String = "Ariful Islam Raju";
+    const bio = (
       <div>
-        <h1>Ariful Islam</h1>
+        <h3>{name}</h3>
+        <p>Fullstack Javascript Developer</p>
+      </div>
+    );
+    return (
+      <div className="App">
+        <h1 {...obj}>My name is {name}</h1>
         <h2>what is JSX</h2>
         <p>JSX is Awesome and it's mean Javascript Extention</p>
+        <p>{new Date().toISOString()}</p>
+        {bio}
       </div>
     );
   }
