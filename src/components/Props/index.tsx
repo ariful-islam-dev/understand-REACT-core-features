9f75602b56fc41a99eed16972301808b985b2289
ariful4082 <ariful4082@gmail.com>
2022-04-06 12:04:13 +0600

    introducing state in react\

--- src/components/Props/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyProps = (props: any) => {
-  return <h1>My Name is {props.name}</h1>;
-};
-
-export default MyProps;
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./app.style.css";
+
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Why do we need state</h1>
+        <h2>Count = {this.state.count}</h2>
+
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          ADD +1
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
