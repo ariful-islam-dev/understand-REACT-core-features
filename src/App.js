7af763c4789a223e0091664989f20db0d577aeb7
ariful4082 <ariful4082@gmail.com>
2022-04-08 19:26:22 +0600

    implement poll crud functionalities

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
 import React from "react";
 import { Col, Container, Row } from "reactstrap";
+import shortid from "shortid";
 import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
+import polls from "./data/polls";
 class App extends React.Component {
- render(){
-  return (
-   <Container className="my-5">
-     <Row>
-       <Col md={4}>
-         <Sidebar/>
-       </Col>
-       <Col md={8}>
-         <MainContent/>
-       </Col>
-     </Row>
-   </Container>
-   );
- }
+  state = {
+    polls: [],
+    selectedPoll: {},
+    searchTerm: "",
+  };
+
+  componentDidMount() {
+    this.setState({ polls: polls });
+  }
+
+  addNewPoll = (poll) => {
+    poll.id = shortid.generate();
+    poll.created = new Date();
+    poll.totalVote = 0;
+    poll.opinions = [];
+
+    this.setState({
+      polls: this.state.polls.concat(poll),
+    });
+  };
+
+  updatePoll = (updatedPoll) => {
+    const polls = [...this.state.polls];
+    const poll = polls.find((p) => p.id === updatedPoll.id);
+
+    poll.title = updatedPoll.title;
+    poll.description = updatedPoll.description;
+    poll.opinions = updatedPoll.opinions;
+
+    this.setState({ polls });
+  };
+
+  deletePoll = (pollId) => {
+    const polls = this.state.polls.filter((p) => p.id !== pollId);
+    this.setState({ polls, selectedPoll: {} });
+  };
+
+  selectPoll = (pollId) => {
+    const poll = this.state.polls.find((p) => p.id === pollId);
+    this.setState({selectedPoll: poll})
+  };
+
+
+  render() {
+    return (
+      <Container className="my-5">
+        <Row>
+          <Col md={4}>
+            <Sidebar />
+          </Col>
+          <Col md={8}>
+            <MainContent />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
